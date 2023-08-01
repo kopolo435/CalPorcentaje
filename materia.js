430802d93b226c2b6bfe6e4c391403f403223f1b
kopolo435 <samirfanilla@gmail.com>
2023-07-31 20:46:32 -0500

    Arregla funcion de evento

--- materia.js
+++ materia.js
@@ -1,6 +1,7 @@
 let mostrarNotaList = document.getElementsByClassName("showNotas");
 
-mostrarNotaList.forEach(button => {
+Array.from(mostrarNotaList).forEach(button => {
+
     button.addEventListener("click", ()=>{
     // Find the parent porcentajeContainer element
     const parentPorcentajeContainer = button.closest(".porcentajeContainer");
@@ -8,6 +9,6 @@ mostrarNotaList.forEach(button => {
     // Find the child notasContainer element within the parent porcentajeContainer
     const notasContainer = parentPorcentajeContainer.querySelector(".notasContainer");
     // Toggle the display property for the notasContainer
-    notasContainer.style.display = notasContainer.style.display === "none" ? "block" : "none";
+    notasContainer.style.display = notasContainer.style.display === "none" ? "grid" : "none";
     })
 });
