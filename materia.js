a08d6b4f9b7823b50c709a83c14b001e5861c091
kopolo435 <samirfanilla@gmail.com>
2023-08-01 09:35:07 -0500

    Agrega funcion que crea label de nombre

--- materia.js
+++ materia.js
@@ -1,6 +1,28 @@
 let mostrarNotaList = document.getElementsByClassName("showNotas");
 const addPorcentajeBtn = document.getElementById("addPorcentaje");
 
+
+function createLabelPor(parent){
+    const label =document.createElement("label");
+    const pNombre = document.createElement("p");
+    const inputNombre = document.createElement("input");
+
+    
+    pNombre.textContent = "Nombre Porcentaje"; 
+
+    inputNombre.setAttribute("name", "nomrePor"); //Coloca los atributos necesarios del input
+    inputNombre.setAttribute("type", "text");   
+    inputNombre.setAttribute("id", "nomrePor");
+    inputNombre.setAttribute("placeholder","Portafolio");
+
+    label.setAttribute("id","labelNombrePor");//Coloca los atributos necesarios del label
+    label.setAttribute("class","inputNew");
+    label.setAttribute("for","nombrePor");
+
+    label.appendChild(pNombre);
+    label.appendChild(inputNombre)
+}
+
 Array.from(mostrarNotaList).forEach(button => {
 
     button.addEventListener("click", ()=>{
