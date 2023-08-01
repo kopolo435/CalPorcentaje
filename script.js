7d041dbb4b44c7479f8f940bfd64088a03529225
kopolo435 <samirfanilla@gmail.com>
2023-07-31 20:39:50 -0500

    Elimina codigo que solo era para materia.html

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let addMateBtn = document.getElementById("addMateria"); //Materia sera abreviado como Mate
 let mateContainer = document.getElementById("listaMaterias");
-let mostrarNotaList = document.getElementsByClassName("showNotas");
 addMateBtn.addEventListener("click", ()=> {
     let materia = document.createElement("div");
     let nombreMate = document.createElement("h2");
@@ -19,15 +18,3 @@ addMateBtn.addEventListener("click", ()=> {
     mateContainer.appendChild(materia)
 
 })
-
-mostrarNotaList.forEach(button => {
-    button.addEventListener("click", ()=>{
-    // Find the parent porcentajeContainer element
-    const parentPorcentajeContainer = button.closest(".porcentajeContainer");
-
-    // Find the child notasContainer element within the parent porcentajeContainer
-    const notasContainer = parentPorcentajeContainer.querySelector(".notasContainer");
-    // Toggle the display property for the notasContainer
-    notasContainer.style.display = notasContainer.style.display === "none" ? "block" : "none";
-    })
-});
