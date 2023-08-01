e0a5b199f1aeb21670cd364da401d06e65dc1d5b
kopolo435 <samirfanilla@gmail.com>
2023-07-31 20:36:44 -0500

    Agrega codigo para mostrar o ocultar notas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let addMateBtn = document.getElementById("addMateria"); //Materia sera abreviado como Mate
 let mateContainer = document.getElementById("listaMaterias");
+let mostrarNotaList = document.getElementsByClassName("showNotas");
 addMateBtn.addEventListener("click", ()=> {
     let materia = document.createElement("div");
     let nombreMate = document.createElement("h2");
@@ -18,3 +19,15 @@ addMateBtn.addEventListener("click", ()=> {
     mateContainer.appendChild(materia)
 
 })
+
+mostrarNotaList.forEach(button => {
+    button.addEventListener("click", ()=>{
+    // Find the parent porcentajeContainer element
+    const parentPorcentajeContainer = button.closest(".porcentajeContainer");
+
+    // Find the child notasContainer element within the parent porcentajeContainer
+    const notasContainer = parentPorcentajeContainer.querySelector(".notasContainer");
+    // Toggle the display property for the notasContainer
+    notasContainer.style.display = notasContainer.style.display === "none" ? "block" : "none";
+    })
+});
