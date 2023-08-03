d354c3e5e952dc9fdbce597dfe3d0439cf5ca3c2
kopolo435 <samirfanilla@gmail.com>
2023-08-03 09:29:20 -0500

    Agrega comentarios al codigo

--- materia.js
+++ materia.js
@@ -1,11 +1,16 @@
-const addPorcentajeBtn = document.getElementById("addPorcentaje");
-const materiasCon = document.getElementById("materiasCon");
-const addNotaList = document.getElementsByClassName("addNota");
-let  eliminarPorBtnLista = document.getElementsByClassName("eliminarPorBtn");
-let eliminarNotaBtnLista = document.getElementsByClassName("eliminarBtn");
-let mostrarNotaList = document.getElementsByClassName("showNotas");
+const addPorcentajeBtn = document.getElementById("addPorcentaje"); //Boton que añade form de nuevo porcentaje
+const materiasCon = document.getElementById("materiasCon");        //Contenedor que almacena los todos los porcentajes
+const addNotaList = document.getElementsByClassName("addNota");    //Lista de botones de añadir una nueva nota
+let  eliminarPorBtnLista = document.getElementsByClassName("eliminarPorBtn"); //Lista de botones de eliminar una porcentaje
+let eliminarNotaBtnLista = document.getElementsByClassName("eliminarBtn");  //Botones para eliminar una nota
+let mostrarNotaList = document.getElementsByClassName("showNotas");  //Botones para mostrar las notas
 
-function createLabelNombrePor(){
+
+/*
+Funcion que crea la label que contiene el input de nombre del porcentaje, devuelve la label
+con todos sus elementos
+*/
+function createLabelNombrePor(){ 
     const label =document.createElement("label");
     const pNombre = document.createElement("p");
     const inputNombre = document.createElement("input");
@@ -28,6 +33,10 @@ function createLabelNombrePor(){
     return label
 }
 
+/*
+Funcion que crea la label que contiene input del valor del porcentaje, devuelve la label
+con todos sus elementos
+*/
 function createLabelValPor(){
     const label = document.createElement("label");
     const pVal = document.createElement("p");
@@ -35,7 +44,7 @@ function createLabelValPor(){
     const container = document.createElement("div");
     const pPorncetaje = document.createElement("p");
 
-    pVal.textContent = "Valor Porcentaje"; 
+    pVal.textContent = "Valor Porcentaje"; //Coloca texto necesario
     pPorncetaje.textContent = "%";
 
     inputVal.setAttribute("name", "valPor"); //Coloca los atributos necesarios del input
@@ -47,13 +56,16 @@ function createLabelValPor(){
     label.setAttribute("class","inputNew");
     label.setAttribute("for","valPor");
 
-    label.appendChild(pVal);
+    label.appendChild(pVal); //Añade los elementos
     container.appendChild(inputVal);
     container.appendChild(pPorncetaje);
     label.appendChild(container);
     return label
 }
 
+/*
+Funcion que crea el form que almacenara las distintas labels
+*/
 function createNewElementForm(){
     const form = document.createElement("form");
 
@@ -62,26 +74,10 @@ function createNewElementForm(){
     return form
 }
 
-Array.from(mostrarNotaList).forEach(button => {
-
-    button.addEventListener("click", ()=>{
-    // Find the parent porcentajeContainer element
-    const parentPorcentajeContainer = button.closest(".porcentajeContainer");
-
-    // Find the child notasContainer element within the parent porcentajeContainer
-    const notasContainer = parentPorcentajeContainer.querySelector(".notasContainer");
-    // Toggle the display property for the notasContainer
-    notasContainer.style.display = notasContainer.style.display === "none" ? "grid" : "none";
-    })
-});
-
-function changeDisabledNota(boolean){
-    const addNotaBtnLista = document.getElementsByClassName("addNota");
-    Array.from(addNotaBtnLista).forEach(button =>{
-        button.disabled = boolean;
-    })
-}
-
+/*
+Crea input que contendra como value el nombre de la materia a la cual se le agrega
+un porcentaje
+*/
 function createNombreMateInput(){
     const input = document.createElement("input");
     const nombreMate = document.getElementById("nombreMate");
@@ -94,6 +90,177 @@ function createNombreMateInput(){
     return input
 }
 
+/*
+Funcion que se encarga de eliminar del contenedor .materiasContainer toda
+la informacion del porcentaje al cual se le hizo click de eliminar.
+Toma como argumento el boton de eliminar y se le agrega el event de click
+*/
+function EliminarPorEvent(button){
+    button.addEventListener("click", ()=>{
+        const eliminarParent = button.parentNode.parentNode;
+        if (eliminarParent.nodeName != "FORM"){
+            eliminarParent.parentNode.removeChild(eliminarParent);
+        }
+        else{
+            eliminarParent.parentNode.parentNode.removeChild(eliminarParent.parentNode);
+            addPorcentajeBtn.disabled = false;
+            changeDisabledNota(false);
+        }
+    })
+}
+
+/*
+Funcion que se encarga de eliminar del contenedor .notasContainer toda
+la informacion de la nota a la cual se le hizo click de eliminar.
+Toma como argumento el boton de eliminar y se le agrega el event de click
+*/
+function eliminarNotaEvent(button){
+    button.addEventListener("click", () =>{
+        const eliminarParent = button.parentNode;
+        if(eliminarParent.nodeName !="FORM"){
+            eliminarParent.parentNode.removeChild(eliminarParent);
+        }
+        else{
+            eliminarParent.parentNode.parentNode.removeChild(eliminarParent.parentNode);
+            changeDisabledNota(false);
+            addPorcentajeBtn.disabled = false;
+        }
+    })
+}
+
+/*
+Crea la label que contiene el input de la nota obtenida, devuelve la label
+*/
+function createLabelNotaObt(){
+    const label = document.createElement("label");
+    const notaObt = document.createElement("p");
+    const inputNotaObt = document.createElement("input");
+
+    notaObt.textContent = "Nota obtenida";
+
+    label.setAttribute("id","labelNotaObt");
+    label.setAttribute("class","notaObt inputNew");
+    label.setAttribute("for","notaObt");
+    inputNotaObt.setAttribute("type","number");
+    inputNotaObt.setAttribute("id","notaObt");
+    inputNotaObt.setAttribute("name","notaObt");
+    inputNotaObt.setAttribute("placeholder","0");
+
+    label.appendChild(notaObt);
+    label.appendChild(inputNotaObt);
+    
+    return label
+
+}
+
+/*
+Crea la label que contiene el input de la nota posible, devuelve la label
+*/
+function createLabelNotaPos(){
+    const label = document.createElement("label");
+    const notaPos = document.createElement("p");
+    const inputNotaPos = document.createElement("input");
+
+    notaPos.textContent = "Nota Posible";
+
+    label.setAttribute("id","labelNotaPos");
+    label.setAttribute("class","notaPos inputNew");
+    label.setAttribute("for","notaPos");
+    inputNotaPos.setAttribute("type","number");
+    inputNotaPos.setAttribute("id","notaPos");
+    inputNotaPos.setAttribute("name","notaPos");
+    inputNotaPos.setAttribute("placeholder","0");
+
+    label.appendChild(notaPos);
+    label.appendChild(inputNotaPos);
+    
+    return label
+
+}
+/*
+Crea la label que contiene el input del nombre de la nota, devuelve la label
+*/
+function createLabelNombreNota(){
+    const label = document.createElement("label");
+    const nombreNota = document.createElement("input");
+    const pNombre = document.createElement("p");
+
+    pNombre.textContent = "Nombre nota"
+
+    label.setAttribute("class","inputNew nombreNota");
+    label.setAttribute("for","nombreNota");
+    nombreNota.setAttribute("type","text");
+    nombreNota.setAttribute("placeholder","Portafolio");
+    nombreNota.setAttribute("name","nombreNota");
+    nombreNota.setAttribute("id","nombreNota");
+
+    label.appendChild(pNombre);
+    label.appendChild(nombreNota);
+
+    return label
+}
+/*
+Crea el input que contendra el nombre del porcentaje al cual se le agrega la nota
+toma como argumento el container de los porcentajes para realizar una busqueda del
+titulo del porcentaje
+*/
+function createNombrePorInput(porContainer){
+    const input = document.createElement("input");
+    let nombrePor = porContainer.getElementsByClassName("infoPorcentaje");
+    nombrePor = nombrePor[0].getElementsByClassName("nombrePorcentaje");
+    nombrePor = nombrePor[0];
+
+    input.setAttribute("type","text");
+    input.setAttribute("name","nombrePor");
+    input.setAttribute("value",nombrePor.textContent);
+    input.hidden = true;
+
+    return input
+}
+/*
+Crea la el elemento que contendra el form de la nota que se quiere crear
+*/
+function createNota(porContainer){
+    const nota = document.createElement("div");
+    const form = document.createElement("form");
+    const eliminarBtn = document.createElement("button");
+    const notaObt = createLabelNotaObt();
+    const notaPos = createLabelNotaPos();
+    const nombreLabel = createLabelNombreNota();
+    const nombrePorInput = createNombrePorInput(porContainer);
+
+    eliminarBtn.textContent = "Eliminar"
+    eliminarNotaEvent(eliminarBtn);
+    
+    nota.setAttribute("class","nota");
+    form.setAttribute("class","nota");
+    eliminarBtn.setAttribute("class","eliminarBtn");
+
+    form.appendChild(eliminarBtn);
+    form.appendChild(nombreLabel);
+    form.appendChild(notaObt);
+    form.appendChild(notaPos);
+    form.append(nombrePorInput);
+    nota.appendChild(form);
+
+    return nota
+}
+
+/*
+Modifica el disabled de los botones de agregar nota, toma como argumento
+un booleano que indica a que estado se quiere colocarlos
+*/
+function changeDisabledNota(boolean){
+    const addNotaBtnLista = document.getElementsByClassName("addNota");
+    Array.from(addNotaBtnLista).forEach(button =>{
+        button.disabled = boolean;
+    })
+}
+
+/*
+Añade evento de click al boton de añadir porcentaje, realiza la creacion de todos los
+elementos necesarios que debe contener el form que se le mostrara al usuario
+*/
 addPorcentajeBtn.addEventListener("click", ()=>{
     const form = createNewElementForm();
     const nombreLabel = createLabelNombrePor();
@@ -121,147 +288,8 @@ addPorcentajeBtn.addEventListener("click", ()=>{
     changeDisabledNota(true);
 })
 
-function EliminarPorEvent(button){
-    button.addEventListener("click", ()=>{
-        const eliminarParent = button.parentNode.parentNode;
-        if (eliminarParent.nodeName != "FORM"){
-            eliminarParent.parentNode.removeChild(eliminarParent);
-        }
-        else{
-            eliminarParent.parentNode.parentNode.removeChild(eliminarParent.parentNode);
-            addPorcentajeBtn.disabled = false;
-            changeDisabledNota(false);
-        }
-    })
-}
-
-function eliminarNotaEvent(button){
-    button.addEventListener("click", () =>{
-        const eliminarParent = button.parentNode;
-        if(eliminarParent.nodeName !="FORM"){
-            eliminarParent.parentNode.removeChild(eliminarParent);
-        }
-        else{
-            eliminarParent.parentNode.parentNode.removeChild(eliminarParent.parentNode);
-            changeDisabledNota(false);
-            addPorcentajeBtn.disabled = false;
-        }
-    })
-}
-
-Array.from(eliminarNotaBtnLista).forEach(eliminarBtn =>{
-    eliminarNotaEvent(eliminarBtn);
-})
-
-Array.from(eliminarPorBtnLista).forEach(eliminarBtn =>{
-
-    EliminarPorEvent(eliminarBtn);
-});
-
-function createLabelNotaObt(){
-    const label = document.createElement("label");
-    const notaObt = document.createElement("p");
-    const inputNotaObt = document.createElement("input");
-
-    notaObt.textContent = "Nota obtenida";
-
-    label.setAttribute("id","labelNotaObt");
-    label.setAttribute("class","notaObt inputNew");
-    label.setAttribute("for","notaObt");
-    inputNotaObt.setAttribute("type","number");
-    inputNotaObt.setAttribute("id","notaObt");
-    inputNotaObt.setAttribute("name","notaObt");
-    inputNotaObt.setAttribute("placeholder","0");
-
-    label.appendChild(notaObt);
-    label.appendChild(inputNotaObt);
-    
-    return label
-
-}
-
-function createLabelNotaPos(){
-    const label = document.createElement("label");
-    const notaPos = document.createElement("p");
-    const inputNotaPos = document.createElement("input");
-
-    notaPos.textContent = "Nota Posible";
-
-    label.setAttribute("id","labelNotaPos");
-    label.setAttribute("class","notaPos inputNew");
-    label.setAttribute("for","notaPos");
-    inputNotaPos.setAttribute("type","number");
-    inputNotaPos.setAttribute("id","notaPos");
-    inputNotaPos.setAttribute("name","notaPos");
-    inputNotaPos.setAttribute("placeholder","0");
-
-    label.appendChild(notaPos);
-    label.appendChild(inputNotaPos);
-    
-    return label
-
-}
-
-function createLabelNombreNota(){
-    const label = document.createElement("label");
-    const nombreNota = document.createElement("input");
-    const pNombre = document.createElement("p");
-
-    pNombre.textContent = "Nombre nota"
-
-    label.setAttribute("class","inputNew nombreNota");
-    label.setAttribute("for","nombreNota");
-    nombreNota.setAttribute("type","text");
-    nombreNota.setAttribute("placeholder","Portafolio");
-    nombreNota.setAttribute("name","nombreNota");
-    nombreNota.setAttribute("id","nombreNota");
-
-    label.appendChild(pNombre);
-    label.appendChild(nombreNota);
-
-    return label
-}
-
-function createNombrePorInput(porContainer){
-    const input = document.createElement("input");
-    let nombrePor = porContainer.getElementsByClassName("infoPorcentaje");
-    nombrePor = nombrePor[0].getElementsByClassName("nombrePorcentaje");
-    nombrePor = nombrePor[0];
-
-    input.setAttribute("type","text");
-    input.setAttribute("name","nombrePor");
-    input.setAttribute("value",nombrePor.textContent);
-    input.hidden = true;
-
-    return input
-}
-
-function createNota(porContainer){
-    const nota = document.createElement("div");
-    const form = document.createElement("form");
-    const eliminarBtn = document.createElement("button");
-    const notaObt = createLabelNotaObt();
-    const notaPos = createLabelNotaPos();
-    const nombreLabel = createLabelNombreNota();
-    const nombrePorInput = createNombrePorInput(porContainer);
-
-    eliminarBtn.textContent = "Eliminar"
-    eliminarNotaEvent(eliminarBtn);
-    
-    nota.setAttribute("class","nota");
-    form.setAttribute("class","nota");
-    eliminarBtn.setAttribute("class","eliminarBtn");
-
-    form.appendChild(eliminarBtn);
-    form.appendChild(nombreLabel);
-    form.appendChild(notaObt);
-    form.appendChild(notaPos);
-    form.append(nombrePorInput);
-    nota.appendChild(form);
-
-    return nota
-}
-
+//Añade a cada elemento del nodeList de añadir nota, la funcionalidad
+//De crear el form mediante un evento
 Array.from(addNotaList).forEach(addBtn =>{
     addBtn.addEventListener("click",()=>{
         const notasContainer = addBtn.closest(".notasContainer");
@@ -275,3 +303,31 @@ Array.from(addNotaList).forEach(addBtn =>{
 
     })
 })
+
+
+/*
+Toma la nodelist de los botones de mostrar notas y les agrega un evento
+click para mostrar o ocultar el display de las notas
+*/
+Array.from(mostrarNotaList).forEach(button => {
+
+    button.addEventListener("click", ()=>{
+    // Find the parent porcentajeContainer element
+    const parentPorcentajeContainer = button.closest(".porcentajeContainer");
+
+    // Find the child notasContainer element within the parent porcentajeContainer
+    const notasContainer = parentPorcentajeContainer.querySelector(".notasContainer");
+    // Toggle the display property for the notasContainer
+    notasContainer.style.display = notasContainer.style.display === "none" ? "grid" : "none";
+    })
+});
+
+//Agrega a los botones de eliminar nota la funcionalidad de eliminar
+Array.from(eliminarNotaBtnLista).forEach(eliminarBtn =>{
+    eliminarNotaEvent(eliminarBtn);
+})
+//Agrega a los botones de eliminar porcentaje la funcionalidad de eliminar
+Array.from(eliminarPorBtnLista).forEach(eliminarBtn =>{
+
+    EliminarPorEvent(eliminarBtn);
+});
